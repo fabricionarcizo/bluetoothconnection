db57ac836f583d9b9de257d443ad57cbb1810909
Fabricio Batista Narcizo <fabn@itu.dk>
2020-03-21 23:46:41 +0100

    Example #11 (Java Version)
    Example of using Android bluetooth sensor to manage the communication between mobile devices, by Mobile App Development BSc. course (Spring 2020)

---
+++ app/src/main/java/dk/itu/moapd/bluetoothconnection/MainFragment.java
@@ -0,0 +1,163 @@
+package dk.itu.moapd.bluetoothconnection;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class MainFragment extends Fragment {
+
+    private BluetoothAdapter mBluetoothAdapter;
+    private Set<BluetoothDevice> mPairedDevices;
+    private List<String> mDevices;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view,
+                              @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mDevices = new ArrayList<>();
+
+        final DeviceAdapter deviceAdapter = new DeviceAdapter();
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(deviceAdapter);
+
+        final ToggleButton toggle_button =
+                view.findViewById(R.id.toggle_button);
+        final Button visible_button =
+                view.findViewById(R.id.visible_button);
+        final Button devices_button =
+                view.findViewById(R.id.devices_button);
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter != null) {
+            toggle_button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    boolean status = toggle_button.isChecked();
+
+                    if (status) {
+                        if (!mBluetoothAdapter.isEnabled()) {
+                            Intent intent = new Intent(
+                                    BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                            startActivity(intent);
+                        }
+                    }
+                    else if (mBluetoothAdapter.isEnabled())
+                        mBluetoothAdapter.disable();
+
+                    visible_button.setEnabled(status);
+                    devices_button.setEnabled(status);
+                }
+            });
+
+            visible_button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(
+                            BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+                    startActivityForResult(intent, 0);
+                }
+            });
+
+            devices_button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mDevices.clear();
+                    mPairedDevices = mBluetoothAdapter.getBondedDevices();
+                    for (BluetoothDevice device : mPairedDevices)
+                        mDevices.add(device.getName() + " (" +
+                                     device.getAddress() + ")");
+                    deviceAdapter.setDevices(mDevices);
+                }
+            });
+
+            mBluetoothAdapter.disable();
+
+        } else {
+            toggle_button.setEnabled(false);
+            visible_button.setEnabled(false);
+            devices_button.setEnabled(false);
+            mDevices.add("This device does NOT support Bluetooth");
+            deviceAdapter.setDevices(mDevices);
+        }
+    }
+
+    private class DeviceAdapter extends RecyclerView.Adapter<DeviceHolder> {
+
+        private List<String> mDevices;
+
+        @NonNull
+        @Override
+        public DeviceHolder onCreateViewHolder(@NonNull ViewGroup parent,
+                                               int viewType) {
+            LayoutInflater inflater =
+                    LayoutInflater.from(parent.getContext());
+            return new DeviceHolder(inflater, parent);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull DeviceHolder holder,
+                                     int position) {
+            if (mDevices != null && mDevices.size() > position) {
+                String device = mDevices.get(position);
+                holder.bind(device);
+            } else
+                holder.bind("No device available");
+        }
+
+        @Override
+        public int getItemCount() {
+            if (mDevices != null)
+                return mDevices.size();
+            return 0;
+        }
+
+        void setDevices(List<String> devices) {
+            mDevices = devices;
+            notifyDataSetChanged();
+        }
+
+    }
+
+    private class DeviceHolder extends RecyclerView.ViewHolder {
+
+        private final TextView mDevice;
+
+        DeviceHolder(@NonNull LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.list_paired_devices, parent, false));
+            mDevice = itemView.findViewById(R.id.device);
+        }
+
+        void bind(String device) {
+            mDevice.setText(device);
+        }
+
+    }
+
+}
